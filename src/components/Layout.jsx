1b34318c7d07389421cd26300b863bf56d4d36d0
odaju <142748592+odaju@users.noreply.github.com>
2025-03-03 12:16:57 +0100

    lagrer

---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+export default function Layout (){
+    return (
+        <body>
+   <header>
+        <nav>
+            <ul id="faneid">
+            </ul>
+        </nav>
+    </header>
+   <main>
+        <section id="whitebox"> 
+        </section>
+    </main>
+    
+</body>
+    )
+}
