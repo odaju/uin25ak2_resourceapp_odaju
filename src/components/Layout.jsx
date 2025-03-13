9c2234224e2717e0a0c939b2c46e074582516944
odaju <142748592+odaju@users.noreply.github.com>
2025-03-13 15:24:14 +0100

    lagrer nye kommentarer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import Nav from "./Nav";
-
+//Laget en container fra linje 5 til 13 som holder på rammen til prosjektet. Og sikret rett nesting via children prop. 
 export default function Layout ({children}){
     return (
      <div id="container">
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
-/**Har endret om til BrowserRouter**/
+/**Har endret om til BrowserRouter på linje 9 og 11**/
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
